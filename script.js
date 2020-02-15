b10d7380c8b1f11d01442be66379999a8883a152
baansuk <60848529+baansuk@users.noreply.github.com>
2020-02-15 22:35:58 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function addFavthings(){
+    var ul = document.getElementById("favlist");
+    var favthing = document.getElementById("favthing");
+    var li = document.createElement("li");
+    li.setAttribute('id',favthing.value);
+    li.appendChild(document.createTextNode('<a href="http://www.google.com/search?q='+favthing.value+'">'+favthing.value+'</a>'));
+    ul.appendChild(li);
+}
